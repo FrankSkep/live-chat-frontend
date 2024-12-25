e81f8ef179efb792dfe4fc8759b9322198038806
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-12-25 00:22:36 -0800

    add apiurl

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,13 +1,8 @@
 import io from 'socket.io-client';
 
 const createSocket = (username: string) => {
-
-  /* Use this for local testing 
-  return io('http://localhost:3034', {
-    query: { username }
-  });
-  */
-	return io('https://live-chat-api-4y0u.onrender.com', {
+	const api = process.env.API_URL || 'http://localhost:3034';
+	return io(api, {
 		query: { username }
 	});
 };
